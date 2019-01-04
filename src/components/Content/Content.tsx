8fdbbdf8f33342a5cc4bd90963135fb30d4a913e
Mark Napthine <mnapthine@gmail.com>
2019-01-04 18:18:37 +0000

    variant needs to be an enum to work like this.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -11,7 +11,8 @@ export interface IContentProps {
 export const Content: React.FunctionComponent<IContentProps> = props => (
   <div {...style('root', {}, props)}>
 
-    <Text as="h1" uppercase bold>Text as h1</Text>
+    <Text as="h1" variant="speakLoud" uppercase bold>Text as h1</Text>
+
     <Text as="h2" variant="speak">My first stylable component</Text>
 
     <Text as="p" truncate>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
